ed2920074b76081d032d811f57132fbcee05c7ad
unknown <jbambrick@tsilhqotinca>
2020-03-23 14:56:02 -0700

    deleted staff component because we have staff directory under the contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SongsComponent } from './songs/songs.component';
 import { VideosComponent } from './videos/videos.component';
-import { StaffComponent } from './staff/staff.component';
 import { TeachersComponent } from './teachers/teachers.component';
 import { LinksComponent } from './links/links.component';
 import { DialectComponent } from './dialect/dialect.component';
@@ -17,7 +16,6 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'songs', component: SongsComponent},
   {path: 'videos', component: VideosComponent},
-  {path: 'staff', component: StaffComponent},
   {path: 'teachers', component: TeachersComponent},
   {path: 'links', component: LinksComponent},
   {path: 'dialect', component: DialectComponent},
